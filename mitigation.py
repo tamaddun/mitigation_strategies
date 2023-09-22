58146293860efec4854fe740acdf6e53db908430
Kazi Tamaddun <52432022+tamaddun@users.noreply.github.com>
2023-09-22 14:19:40 -0700

    Update mitigation.py

--- mitigation.py
+++ mitigation.py
@@ -11,6 +11,7 @@ landscape2 = Image.open("./Images_resized/landscape2.png").convert("RGBA")
 facility1 = Image.open("./Images_resized/facility1.png").convert("RGBA")
 facility2 = Image.open("./Images_resized/facility2.png").convert("RGBA")
 fence = Image.open("./Images_resized/fence.png").convert("RGBA")
+turf = Image.open("./Images_resized/turf.png").convert("RGBA")
 casks = Image.open("./Images_resized/casks.png").convert("RGBA")
 pole = Image.open("./Images_resized/pole.png").convert("RGBA")
 light = Image.open("./Images_resized/light.png").convert("RGBA")
@@ -55,20 +56,33 @@ st.markdown(
 )
 
 # Add a title to the Streamlit sidebar and customize its font size
-st.sidebar.markdown("<h1 style='font-size: 24px;'>Customize Scene</h1>", unsafe_allow_html=True)
+# st.sidebar.markdown("<h1 style='font-size: 24px;'>Customize Scene</h1>", unsafe_allow_html=True)
 
 # Create a dropdown menu to select a plant type
+st.sidebar.header("Customize Scene")
 selected_landscape = st.sidebar.selectbox("Choose Landscape Type", ["Forest", "Desert"])
-selected_facility = st.sidebar.selectbox("Choose Building Type", ["Wide", "Narrow"])
+selected_facility = st.sidebar.selectbox("Choose Facility Type", ["Wide", "Narrow"])
 selected_vegetation = st.sidebar.selectbox("Choose Vegetation Type", ["Conifers", "Succulent"])
+
+# Display the composite image using Streamlit and customize its font size
+st.markdown("<h1 style='font-size: 32px; text-align: center;'>Visual Impact Mitigation Strategies</h1>", unsafe_allow_html=True)
+
+col1, col2, col3, col4 = st.columns([0.5,1,1,0.5])
+
+with col2:
+    # st.sidebar.header("Customize Facility")
+    selected_texture_facility = st.selectbox("Choose Facility Texture", ["Texture 1", "Texture 2", "Texture 3"])
+with col3:
+    # st.sidebar.header("Customize Grass")
+    selected_grass_turf = st.selectbox("Choose Grass Texture", ["Grass 1", "Grass 2", "Grass 3"])
 
 # Create color pickers to set colors to images
 # facility_color = st.sidebar.color_picker("Choose Facility Color", "#FFFFFF")  # Default color is white
+st.sidebar.header("Customize Color")
+equip_color = st.sidebar.color_picker("Choose Equipment Color", '#f36e21')  # Default color is white
 fence_color = st.sidebar.color_picker("Choose Fence Color", '#FFFFFF')  # Default color is white
-casks_color = st.sidebar.color_picker("Choose Casks Color", '#ECFBFF')  # Default color is white
+# casks_color = st.sidebar.color_picker("Choose Casks Color", '#ECFBFF')  # Default color is white
 light_color = st.sidebar.color_picker("Choose Light", '#FFFF00')  # Default color is white
-lift_color = st.sidebar.color_picker("Choose Forklift Color", '#f36e21')  # Default color is white
-truck_color = st.sidebar.color_picker("Choose Truck Color", '#FF9C10')  # Default color is white
 
 # Create a blank canvas with the size of the first image (ground in this case)
 composite = Image.new("RGBA", ground.size, (135, 206, 235, 255))
@@ -91,14 +105,15 @@ landscape2_recolored = recolor_image(landscape2, "#858585")
 facility1_recolored = recolor_image(facility1, "#FFFFFF")
 facility2_recolored = recolor_image(facility2, "#FFFFFF")
 fence_recolored = recolor_image(fence, fence_color)
-casks_recolored = recolor_image(casks, casks_color)
+turf_recolored = recolor_image(turf, "#FFFFFF")
+casks_recolored = recolor_image(casks, '#FFFFFF')
 pole_recolored = recolor_image(pole, "#808080")
 light_recolored = recolor_image(light, light_color)
 shaft_recolored = recolor_image(shaft, '#FFFFFF')
-lift_recolored = recolor_image(lift, lift_color)
+lift_recolored = recolor_image(lift, equip_color)
 engine_recolored = recolor_image(engine, '#FFFFFF')
-truck_recolored = recolor_image(truck, truck_color)
-wheels_recolored = recolor_image(wheels, '#FFFFFF')
+truck_recolored = recolor_image(truck, equip_color)
+wheels_recolored = recolor_image(wheels, '#929191')
 plant1_recolored = recolor_image(plant1, '#7ac143')
 plant2_recolored = recolor_image(plant2, '#7ac143')
 cloud_recolored = recolor_image(cloud, '#FFFFFF')
@@ -150,26 +165,37 @@ textures = {
     "Texture 3": "./textures/texture3.jpg",
 }
 # Apply texture to facility1
-st.sidebar.header("Customize Facility")
-selected_texture_facility = st.sidebar.selectbox("Choose Texture for Facility", ["Texture 1", "Texture 2", "Texture 3"])
+
 texture_img_facility = cv2.imread(textures[selected_texture_facility], cv2.IMREAD_UNCHANGED)
 texture_img_facility = texture_img_facility[:, :, ::-1]  # BGR to RGB conversion
 facility1_recolored = apply_texture(facility1_recolored, texture_img_facility)
 facility2_recolored = apply_texture(facility2_recolored, texture_img_facility)
+
 if selected_facility == "Wide":
     composite = Image.alpha_composite(composite, facility2_recolored)
 elif selected_facility == "Narrow":
     composite = Image.alpha_composite(composite, facility1_recolored)
+
+# Create a dictionary to map texture names to file paths (assuming textures are in jpg format)
+grass = {
+    "Grass 1": "./textures/grass1.jpg",
+    "Grass 2": "./textures/grass2.jpg",
+    "Grass 3": "./textures/grass3.jpg"
+}
+# Apply grass to turf
+grass_img_turf = cv2.imread(grass[selected_grass_turf], cv2.IMREAD_UNCHANGED)
+# Perform BGR to RGB conversion on the loaded image
+grass_img_turf = cv2.cvtColor(grass_img_turf, cv2.COLOR_BGR2RGB)
+turf_recolored = apply_texture(turf_recolored, grass_img_turf)
 
 if selected_vegetation == "Conifers":
     composite = Image.alpha_composite(composite, plant1_recolored)
 elif selected_vegetation == "Succulent":
     composite = Image.alpha_composite(composite, plant2_recolored)
 composite = Image.alpha_composite(composite, frame_recolored)
+composite = Image.alpha_composite(composite, turf_recolored)
 composite = Image.alpha_composite(composite, text_recolored)
 
-# Display the composite image using Streamlit and customize its font size
-st.markdown("<h1 style='font-size: 32px; text-align: center;'>Visual Impact Mitigation Strategies</h1>", unsafe_allow_html=True)
 # st.image(composite, caption="Illustrated CISF", use_column_width=True)
 st.image(composite, use_column_width=True)
 
